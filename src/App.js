fc552d026e0fac4d46d86527746923ca0d01514a
Aravind-psiog <aravind.prabaharan@psiog.com>
2022-11-03 19:16:42 +0530

    basic react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BaseTemplate from './components/js/BaseTemplate';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseTemplate></BaseTemplate>
+    </div>
+  );
+}
+
+export default App;
