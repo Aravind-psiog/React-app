360bdf85785a6d1f27b316ebc891e2ca68255326
Aravind <100823292+Aravind-psiog@users.noreply.github.com>
2023-06-21 21:05:54 +0530

    Added search bar

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ import SearchBarTemplate from "./components/SearchBar/SearchBar";
 import React, { useState, useEffect } from "react";
 import LocalMoviesRoundedIcon from "@mui/icons-material/LocalMoviesRounded";
 import Button from "@mui/material/Button";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
 
 const api = axios.create({
   baseURL: ApiConfigs.baseURL,
@@ -29,17 +32,33 @@ const api = axios.create({
 function App() {
   return (
     <div className="App">
-      <AppBar position="relative">
-        <Toolbar>
+      <AppBar position="sticky">
+        <Toolbar sx={{ color: "red" }}>
           <LocalMoviesRoundedIcon />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             MMDB
           </Typography>
+          <TextField
+            sx={{
+              width: { sm: 200, md: 300 },
+              "& .MuiInputBase-root": {
+                height: 30,
+              },
+            }}
+            id="standard-basic"
+            label="Search"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-      <HeaderTemplate></HeaderTemplate>
-      <SearchBarTemplate />
       <section className="hero-section">
         <div className="card-grid">
           {MoviesApi().map((movie) => (
